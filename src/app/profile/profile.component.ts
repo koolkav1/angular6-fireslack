269bf17f7a4b27fef28cd24aaa7e872fcb401624
Kav Khalsa <kenny.khalsa@gmail.com>
2018-09-03 16:05:14 +0100

    Able to edit display name

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { NgForm } from '@angular/forms';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+ displayName: string;
+ private readonly notifier: NotifierService;
+  constructor(
+    private userService: UserService,
+    private notifierService: NotifierService) { 
+      this.notifier = this.notifierService;
+    }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {  
+  }
+  updateUser(form: NgForm){
+   this.displayName = form.value.displayName;
+   this.userService.updateUserDisplayName(this.displayName);
+   this.notifier.notify('success', 'You have successfully changed your Display Name');
+   
   }
 
 }
